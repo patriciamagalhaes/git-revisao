0a2f1d1ce6c404ebce7bfefd1bf16a7018bbe007
Patrícia Magalhães <patricia17magalhaes@gmail.com>
2021-03-23 13:54:09 -0300

    version 1.0

---
+++ back.js
@@ -0,0 +1,38 @@
+<!--Materialize JS -->
+    <script src="js/materialize.min.js"></script>
+    <script>
+        
+        document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, options);
+      });
+        
+        $(document).ready(function(){
+            $('.sidenav').sidenav();
+          }); 
+        
+
+        document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.slider');
+        var instances = M.Slider.init(elems, options);
+          });
+
+          // Or with jQuery
+
+        $(document).ready(function(){
+        $('.slider').slider();
+          }); 
+        
+        
+        document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.carousel');
+        var instances = M.Carousel.init(elems, options);
+        });
+
+        // Or with jQuery
+
+       $(document).ready(function(){
+        $('.carousel').carousel();
+        });
+           
+    </script> 
---
+++ front.js
@@ -0,0 +1,43 @@
+/* SEÇÃO--------------------------------------------------------------------------------------MENU */
+
+body {
+    font-family: 'Roboto', sans-serif;
+    /* background: #e1dfdf;  
+    margin: 0px; 
+    padding: 0px;*/
+}
+
+nav {
+    /* background-color: #162028;*/
+    height: 65px;
+    /*position: relative; 
+    text-align:left;*/
+}
+
+nav ul {
+    margin-left: 20px;
+}
+
+nav ul li {
+    display: inline;
+    list-style: none;
+    margin-left: 20px;
+    /*padding: 15px;*/
+}
+
+nav ul li a {
+    color: white;
+    /*padding: 15px;*/
+    text-decoration: none;
+    /*text-transform: uppercase;*/
+    font-size: 15px;
+    margin-right: 10px;
+    /*margin-top: 15px;*/
+}
+
+nav a:hover {
+    background: rgba(0, 0, 0, 0.5);
+    background-color: #212121;
+    border-radius: 5px;
+}
+
